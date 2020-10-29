9ab2ec168165d88ecc76857f356602fc257b2d74
Brandon Pitman <brandon.pitman@gmail.com>
2020-10-28 21:18:59 -0700

    Build script: delete generated-code (proto) directory with each build.
    This will make repeated builds more "hermetic", I suppose.

--- build.rs
+++ build.rs
@@ -1,19 +1,23 @@
 extern crate protoc_rust;
 
 use std::fs;
+use std::io::ErrorKind;
 
 fn main() {
-	fs::create_dir_all("src/proto")
-		.expect("Creating src/proto failed.");
+	let err = fs::remove_dir_all("src/proto");
+	if let Err(ref e) = err {
+		match e.kind() {
+			ErrorKind::NotFound => (), // ignore not-found error
+			_ => err.expect("Removing src/proto failed."),
+		}
+	}
+	fs::create_dir_all("src/proto").expect("Creating src/proto failed.");
 
 	protoc_rust::Codegen::new()
 		.out_dir("src/proto")
-		.inputs(&[
-			"proto/secret/entry.proto",
-			"proto/secret/key.proto",
-		])
+		.inputs(&["proto/secret/entry.proto", "proto/secret/key.proto"])
 		.include("proto")
-		.customize(protoc_rust::Customize{
+		.customize(protoc_rust::Customize {
 			gen_mod_rs: Some(true),
 			..Default::default()
 		})
